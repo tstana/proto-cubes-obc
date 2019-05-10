028d8d1bbad46a0527e46a79328a14fa4cd0e614
Theodor Stana <stana@kth.se>
2019-05-10 14:12:59 +0200

    Commented out running REQ_PAYLOAD every 60 seconds

--- obcsim/cubes_configuration.cpp
+++ obcsim/cubes_configuration.cpp
@@ -34,15 +34,16 @@ void sequence_init(msp_link_t *lnk)
 
 void sequence_loop(msp_link_t *lnk)
 {
-  /*Serial.println("-------- Invoking SEND_CITI_CONF --------");
-    invoke_send(lnk, MSP_OP_SEND_CITI_CONF, data_SEND_CITI_CONF, sizeof(data_SEND_CITI_CONF), NONE);
-    Serial.println("-----------------------------------------\n");
-    delay(5000); */
-    if(RTC_time_since_last(REQUEST_TIME)){
-      Serial.println("-------- Invoking REQ_PAYLOAD --------");
-      invoke_request(lnk, MSP_OP_REQ_PAYLOAD, recv_buf, &buf_len, STRING);
-      RS_send(recv_buf, buf_len);
-      SD_send(recv_buf, buf_len);
-      Serial.println("--------------------------------------\n");
-    }
+//    Serial.println("-------- Invoking SEND_CITI_CONF --------");
+//    invoke_send(lnk, MSP_OP_SEND_CITI_CONF, data_SEND_CITI_CONF, sizeof(data_SEND_CITI_CONF), NONE);
+//    Serial.println("-----------------------------------------\n");
+//    delay(5000);
+//
+//    if(RTC_time_since_last(REQUEST_TIME)){
+//      Serial.println("-------- Invoking REQ_PAYLOAD --------");
+//      invoke_request(lnk, MSP_OP_REQ_PAYLOAD, recv_buf, &buf_len, STRING);
+//      RS_send(recv_buf, buf_len);
+//      SD_send(recv_buf, buf_len);
+//      Serial.println("--------------------------------------\n");
+//    }
 }
